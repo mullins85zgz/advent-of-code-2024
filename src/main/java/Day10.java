2e5668fc072dfc001f2123eca0bc230a03d943f7
Jose Angel "Casa <jangelsalinas@gmail.com>
2024-12-11 19:51:48 +0100

    Implement Day 10 solution for Advent of Code 2024, adding trailhead detection and scoring logic for reachable 9-height positions

--- src/main/java/Day10.java
+++ src/main/java/Day10.java
@@ -16,13 +16,26 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class Day10 {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         int[][] map = readMap();
         int score = 0;
+
+        // Given the map, we need to find the trailheads.
+        // A trailhead is any position that starts one or more hiking trails - here,
+        // these positions will always have height 0.
+        // Assembling more fragments of pages, you establish that a trailhead's score is
+        // the number of 9-height positions reachable from that trailhead via a hiking
+        // trail.
+
+        // We need to find the trailheads
+        // We need to find the 9-height positions reachable from that trailhead via a
+        // hiking trail
+        // We need to calculate the score of each trailhead
+
         for (int i = 0; i < map.length; i++) {
-            for (int j = 0; j < map[0].length; j++) {
+            for (int j = 0; j < map[i].length; j++) {
                 if (map[i][j] == 0) {
-                    score += countPaths(map, i, j);
+                    score += findTrails(map, i, j);
                 }
             }
         }
@@ -31,38 +44,44 @@ public class Day10 {
 
     }
 
-    public static int countPaths(int[][] map, int i, int j) {
-        int count = 0;
-        if (map[i][j] == 9) {
-            return 1;
-        }
-        if (map[i][j] == 0) {
-            map[i][j] = 1;
-            if (i > 0 && map[i - 1][j] == 1) {
-                count += countPaths(map, i - 1, j);
-            }
-            if (i < map.length - 1 && map[i + 1][j] == 1) {
-                count += countPaths(map, i + 1, j);
-            }
-            if (j > 0 && map[i][j - 1] == 1) {
-                count += countPaths(map, i, j - 1);
-            }
-            if (j < map[0].length - 1 && map[i][j + 1] == 1) {
-                count += countPaths(map, i, j + 1);
-            }
-            map[i][j] = 0;
-        }
-        return count;
+    public static int findTrails(int[][] map, int i, int j) {
+        boolean[][] visited = new boolean[map.length][map[0].length];
+        boolean[][] reachedNine = new boolean[map.length][map[0].length];
+        return findTrailsHelper(map, i, j, visited, reachedNine);
     }
 
+    private static int findTrailsHelper(int[][] map, int i, int j, boolean[][] visited, boolean[][] reachedNine) {
+        int score = 0;
+        int[] dx = { 0, 0, 1, -1 };
+        int[] dy = { 1, -1, 0, 0 };
 
+        visited[i][j] = true;
+
+        for (int k = 0; k < 4; k++) {
+            int x = i + dx[k];
+            int y = j + dy[k];
+
+            if (x >= 0 && x < map.length && y >= 0 && y < map[x].length && !visited[x][y]) {
+                if (map[x][y] == 9 && map[i][j] == 8 && !reachedNine[x][y]) {
+                    score++;
+                    reachedNine[x][y] = true;
+                    // System.out.println("Reached 9 at: " + x + ", " + y);
+                } else if (map[x][y] == map[i][j] + 1) {
+                    score += findTrailsHelper(map, x, y, visited, reachedNine);
+                }
+            }
+        }
+
+        visited[i][j] = false;
+        return score;
+    }
 
     public static int[][] readMap() {
-        
+
         int lineSize = 0;
         ArrayList<String> lines = new ArrayList<>();
 
-        try (BufferedReader br = new BufferedReader(new FileReader("res/Day10_inputTest.txt"))) {
+        try (BufferedReader br = new BufferedReader(new FileReader("res/Day10_input.txt"))) {
             String line;
             while ((line = br.readLine()) != null) {
                 lines.add(line);
